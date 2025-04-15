dae4b3fa7fca48b46c7be9cba73972d84eb444d2
shakil <kazimuhammadullah@gmail.com>
2025-04-16 02:50:43 +0600

    project initiet

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -21,17 +21,17 @@
 <!-- /.content-header -->
 
   <!-- content area -->
-  <section class="content d-none">
+  <section class="content">
     <div class="container-fluid">
       <!-- Small boxes (Stat box) -->
       <div class="row">
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <!-- small box -->
           <div class="small-box bg-info">
             <div class="inner">
               <h3>150</h3>
 
-              <p>New Orders</p>
+              <p>New Clients</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
@@ -40,13 +40,13 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <!-- small box -->
           <div class="small-box bg-success">
             <div class="inner">
               <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-              <p>Bounce Rate</p>
+              <p>Processing Clients</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
@@ -55,13 +55,13 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <!-- small box -->
           <div class="small-box bg-warning">
             <div class="inner">
               <h3>44</h3>
 
-              <p>User Registrations</p>
+              <p>Complete Clients</p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
@@ -70,13 +70,28 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <!-- small box -->
           <div class="small-box bg-danger">
             <div class="inner">
               <h3>65</h3>
 
-              <p>Unique Visitors</p>
+              <p>Decline Clients</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-primary">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Due Amount</p>
             </div>
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
@@ -88,7 +103,7 @@
       </div>
       <!-- /.row -->
       <!-- Main row -->
-      <div class="row">
+      <div class="row d-none">
         <!-- Left col -->
         <section class="col-lg-7 connectedSortable">
           
--- resources/views/manager/inc/sidebar.blade.php
+++ resources/views/manager/inc/sidebar.blade.php
@@ -8,13 +8,58 @@
         <a href="{{route('manager.dashboard')}}" class="nav-link {{ (request()->is('manager/dashboard*')) ? 'active' : '' }}">
           <i class="nav-icon fas fa-th"></i>
           <p>
-            Dashboard
+            Add Client
+          </p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="{{route('manager.dashboard')}}" class="nav-link {{ (request()->is('manager/dashboard*')) ? 'active' : '' }}">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            New Client
+          </p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="{{route('manager.processingclient')}}" class="nav-link {{ (request()->is('manager/processing-clients*')) ? 'active' : '' }}">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Processing Client
+          </p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="{{route('manager.completedclient')}}" class="nav-link {{ (request()->is('manager/completed-clients*')) ? 'active' : '' }}">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Complete Client
+          </p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="{{route('manager.declineclient')}}" class="nav-link {{ (request()->is('manager/decline-clients*')) ? 'active' : '' }}">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Decline Client
+          </p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="{{route('manager.dashboard')}}" class="nav-link {{ (request()->is('manager/dashboard*')) ? 'active' : '' }}">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Due Amount
           </p>
         </a>
       </li>
 
       
-      <li class="nav-item {{ (request()->is('manager/client*')) ? 'menu-open' : '' }}{{ (request()->is('manager/completed-clients*')) ? 'menu-open' : '' }}{{ (request()->is('manager/decline-clients*')) ? 'menu-open' : '' }}{{ (request()->is('manager/processing-clients*')) ? 'menu-open' : '' }}">
+      {{-- <li class="nav-item {{ (request()->is('manager/client*')) ? 'menu-open' : '' }}{{ (request()->is('manager/completed-clients*')) ? 'menu-open' : '' }}{{ (request()->is('manager/decline-clients*')) ? 'menu-open' : '' }}{{ (request()->is('manager/processing-clients*')) ? 'menu-open' : '' }}">
         <a href="#" class="nav-link {{ (request()->is('manager/client*')) ? 'active' : '' }}">
           <i class="nav-icon fas fa-copy"></i>
           <p>
@@ -23,24 +68,6 @@
           </p>
         </a>
         <ul class="nav nav-treeview">
-          <li class="nav-item">
-            <a href="{{route('manager.processingclient')}}" class="nav-link {{ (request()->is('manager/processing-clients*')) ? 'active' : '' }}">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Processing</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{route('manager.completedclient')}}" class="nav-link {{ (request()->is('manager/completed-clients*')) ? 'active' : '' }}">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Complete</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{route('manager.declineclient')}}" class="nav-link {{ (request()->is('manager/decline-clients*')) ? 'active' : '' }}">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Decline</p>
-            </a>
-          </li>
           
           <li class="nav-item">
             <a href="{{route('manager.client')}}" class="nav-link {{ (request()->is('manager/client*')) ? 'active' : '' }}">
@@ -50,7 +77,7 @@
           </li>
 
         </ul>
-      </li>
+      </li> --}}
 
 
 
